0e9022232871eb6213c14a9515c5ceb88f34c368
Caue Silva <cauecatonesilva@gmail.com>
2023-11-08 11:41:39 -0400

    featch: refazer deploy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import { router } from './router/router';
 import  express  from 'express';
+import cors from 'cors';
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+
 app.use(express.json());
 app.use(router)
 app.listen(process.env.PORT || 5000, () => {
